cfe03d00521d2ae3f5354dc3db3b9339d0e740f2
Abdul and Glenn <glenn.tigas@gmail.com>
2017-09-19 13:30:54 -0400

    Completes CSS Box exercise and begins work on AA questions SQL exercise

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,81 @@
+DROP TABLE users;
+DROP TABLE questions;
+DROP TABLE question_follows;
+DROP TABLE question_likes;
+DROP TABLE replies;
+
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL  ,
+  parent_reply_id INTEGER,
+  user_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Abdul', 'Mohamed'),
+  ('Glenn', 'Tigas');
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('Why?', 'WHy is the sky blue?', (SELECT id FROM users WHERE fname = 'Glenn')),
+  ('Whyyy?', 'WHyyyyy is the sky blue?', (SELECT id FROM users WHERE fname = 'Glenn'));
+
+INSERT INTO
+  question_follows (question_id, user_id)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'Why?'), (SELECT id FROM users WHERE fname = 'Abdul'));
+
+INSERT INTO
+  replies (question_id, parent_reply_id, user_id, body)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'Why?'), NULL, (SELECT id FROM users WHERE fname = 'Abdul'), 'Wow'),
+  ((SELECT id FROM questions WHERE title = 'Why?'), 1, (SELECT id FROM users WHERE fname = 'Abdul'), 'Cool'),
+  ((SELECT id FROM questions WHERE title = 'Why?'), 2, (SELECT id FROM users WHERE fname = 'Glenn'), ' :)');
+
+INSERT INTO
+  question_likes (question_id, user_id)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'Why?'), (SELECT id FROM users WHERE fname = 'Abdul'));
---
+++ aa_questions/replies.rb
@@ -0,0 +1,81 @@
+require_relative 'QuestionsDatabase'
+
+class Reply
+  attr_accessor :id, :question_id, :parent_reply_id, :user_id, :body
+
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+
+    Reply.new(reply.first)
+  end
+  def self.find_by_parent_id(parent_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, parent_id)
+    SELECT
+      *
+    FROM
+      replies
+    WHERE
+      parent_id = ?
+  SQL
+    replies.map{|reply| Reply.new(reply) }
+  end
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @parent_reply_id = options['parent_reply_id']
+    @user_id = options['user_id']
+    @body = options['body']
+  end
+
+  def author
+    User.find_by_id(@user_id)
+
+  end
+
+  def question
+    Question.find_by_id(@question_id)
+
+  end
+
+  def parent_reply
+    Reply.find_by_id(@parent_reply_id)
+  end
+
+  def child_replies
+    Reply.find_by_parent_id(@id)
+  end
+
+end
