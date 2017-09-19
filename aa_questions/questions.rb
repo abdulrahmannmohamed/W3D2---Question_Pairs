a1b9982504d11bee3cac3b51675e3db2f71acc41
Abdul-Rahman Mohamed <abdulrahman.abdelaziz@gmail.com>
2017-09-19 13:30:54 -0400

    Completes CSS Box exercise and begins work on AA questions SQL exercise

---
+++ aa_questions/questions.rb
@@ -0,0 +1,45 @@
+require_relative 'QuestionsDatabase'
+
+class Question
+  attr_accessor :id, :title, :body, :author_id
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM
+      questions
+    WHERE
+     id = ?
+    SQL
+    Question.new(question.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+    SELECT
+      *
+    FROM
+      questions
+    WHERE
+     author_id = ?
+    SQL
+
+    questions.map {|question| Question.new(question)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def author
+    User.find_by_id(@author_id)
+
+  end
+
+  def replies
+    Reply.find_by_question_id(@id)
+  end
+end
